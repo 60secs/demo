592f26c7d29132d849e34def48af5655bb27ab27
Eric Driggs <ericdriggs@hotmail.com>
2013-06-24 23:10:30 -0700

    lint

--- write-lock-fix/write-lock-fix/test/sixtysecs/db/WriteSkewFixTest.java
+++ write-lock-fix/write-lock-fix/test/sixtysecs/db/WriteSkewFixTest.java
@@ -7,11 +7,9 @@ import java.sql.SQLRecoverableException;
 import org.junit.Test;
 
 public class WriteSkewFixTest {
-
-	private final static String connectionString = "jdbc:sqlserver://127.0.0.1;databaseName=write_skew;user=sa;password=abc123;";
-	private final static String existingTableName = "foo";
-	// = ""; // TODO:
-	private final static String lockName = "lock12345";
+	private final static String connectionString = "jdbc:sqlserver://127.0.0.1;databaseName=POS;user=demo;password=demo;";
+	private final static String existingTableName = "configuration_table";
+	
 	private final static KeyedSerializedConnectionFactory writeSkewFix;
 
 	static {
@@ -25,15 +23,17 @@ public class WriteSkewFixTest {
 		writeSkewFix = tmpWriteSkewFix;
 	}
 
+	
 	@Test
 	public void expectOneTransactionHasNoErrors() throws Exception {
-		Connection transaction = null;
+		Connection con = null;
+		String lockName = "lock12345";
 
 		try {
-			transaction = writeSkewFix.getKeyedSerializedConnection(lockName);
+			con = writeSkewFix.getKeyedSerializedConnection(lockName);
 		} finally {
-			if (transaction != null) {
-				transaction.rollback();
+			if (con != null) {
+				con.rollback();
 			}
 		}
 	}
@@ -44,6 +44,8 @@ public class WriteSkewFixTest {
 		Connection transaction = null;
 		Connection transaction2 = null;
 
+		String lockName = "lock123456";
+		
 		try {
 			transaction = writeSkewFix.getKeyedSerializedConnection(lockName);
 			transaction2 = writeSkewFix.getKeyedSerializedConnection(lockName);
